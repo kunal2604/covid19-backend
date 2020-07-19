1de8543e3e8429b35cdaeaf50a80283ee640a596
Kunal Kumar <kmrknl2604@gmail.com>
2020-07-19 17:04:20 +0530

    Added CORS headers

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const EXPRESS = require('express');
 
 const APP = EXPRESS();
 
+APP.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS');
+    next();
+});
 APP.use('/api/posts', (req, res, next) => {
     const POSTS = [
         {
