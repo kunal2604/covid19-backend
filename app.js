1e47cffba2b6b3372c1b871b638c928acee48597
Kunal Kumar <kmrknl2604@gmail.com>
2020-07-19 16:48:08 +0530

    Dummy GET request to fetch posts

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const EXPRESS = require('express');
 
 const APP = EXPRESS();
 
-APP.use((req, res, next) => {
-    console.log('First middleware')/
-    next();
-});
-
-APP.use((req, res, next) => {
-    res.send('Hello from Express');
+APP.use('/api/posts', (req, res, next) => {
+    const POSTS = [
+        {
+            id: "fadf234hj",
+            title: "First dummy post from server",
+            content: "Hello. This is coming from the backend. But this is purely dummy data"
+        },
+        {
+            id: "kjhk24",
+            title: "Second dummy post from server",
+            content: "Hello again!! Thiis is also dummy data"
+        }
+    ]
+     res.status(200).json({
+         message: 'Posts fetched successfully!',
+         posts: POSTS
+     });
 });
 
 module.exports = APP;  
