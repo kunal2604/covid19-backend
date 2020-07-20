e93bd8ca07ce10298c1955ff78d98b066818cd4a
Kunal Kumar <kmrknl2604@gmail.com>
2020-07-21 03:42:47 +0530

    Reorganized backend routes - I

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const EXPRESS = require('express');
 const APP = EXPRESS();
-const Post = require('./models/post');
 const MONGOOSE = require('mongoose');
 
+const POST_ROUTER = require('./routes/posts');
 const MONGO_SERVER = "mongodb+srv://kunal2604:PyeFWckO3UWFSQ1f@cluster2604.u09yn.mongodb.net/covid19?retryWrites=true&w=majority";
 
 MONGOOSE.connect(
@@ -30,65 +30,6 @@ APP.use((req, res, next) => {
     next();
 });
 
-APP.post('/api/posts', (req, res, next) => {
-    const POST = new Post({
-        title: req.body.title,
-        content: req.body.content
-    });
-    console.log(POST);
-    POST.save().then(createdPost => {
-        res.status(201).json({
-            message: 'Post added successfully',
-            postId: createdPost._id
-        });
-    })
-    // 201 --> everything OK, a new resource was created
-    
-});
-
-APP.get('/api/posts', (req, res, next) => {
-    Post.find().then(documents => {
-        console.log(documents);
-        // 200 --> everything OK
-        res.status(200).json({
-        message: 'Posts fetched successfully!',
-        posts: documents
-        });
-    });
-});
-
-APP.get('/api/posts/:id', (req, res, next) => {
-    Post.findById(req.params.id).then(post => {
-        if(post) {
-            res.status(200).json(post);
-        } else {
-            res.status(404).json({ message: "Post not found!" });
-        }
-    })
-});
-
-APP.delete("/api/posts/:id", (req, res, next) => {
-    console.log('Deleting -> ', req.params.id);
-    Post.deleteOne({ _id: req.params.id })
-        .then(result => {
-            console.log(result);
-            res.status(200).json({ message: "Post deleted!" });
-        });
-    
-});
-
-// APP.put() -> To put new resource and completely replace the old one
-// APP.patch() -> To only update existing resource with new value
-APP.put("/api/posts/:id", (req, res, next) => {
-    const POST = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content
-    });
-    Post.updateOne({_id: req.params.id}, POST)
-        .then(result => {
-            res.status(200).json({ message: "Update successful"});
-        });
-});
+APP.use(POST_ROUTER);
 
 module.exports = APP;  
---
+++ routes/posts.js
@@ -0,0 +1,67 @@
+const EXPRESS = require('express');
+const ROUTER = EXPRESS.Router();
+
+const Post = require('../models/post');
+
+ROUTER.post('/api/posts', (req, res, next) => {
+    const POST = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+    console.log(POST);
+    POST.save().then(createdPost => {
+        res.status(201).json({
+            message: 'Post added successfully',
+            postId: createdPost._id
+        });
+    })
+    // 201 --> everything OK, a new resource was created
+    
+});
+
+ROUTER.get('/api/posts', (req, res, next) => {
+    Post.find().then(documents => {
+        console.log(documents);
+        // 200 --> everything OK
+        res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: documents
+        });
+    });
+});
+
+ROUTER.get('/api/posts/:id', (req, res, next) => {
+    Post.findById(req.params.id).then(post => {
+        if(post) {
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({ message: "Post not found!" });
+        }
+    })
+});
+
+ROUTER.delete("/api/posts/:id", (req, res, next) => {
+    console.log('Deleting -> ', req.params.id);
+    Post.deleteOne({ _id: req.params.id })
+        .then(result => {
+            console.log(result);
+            res.status(200).json({ message: "Post deleted!" });
+        });
+    
+});
+
+// APP.put() -> To put new resource and completely replace the old one
+// APP.patch() -> To only update existing resource with new value
+ROUTER.put("/api/posts/:id", (req, res, next) => {
+    const POST = new Post({
+        _id: req.body.id,
+        title: req.body.title,
+        content: req.body.content
+    });
+    Post.updateOne({_id: req.params.id}, POST)
+        .then(result => {
+            res.status(200).json({ message: "Update successful"});
+        });
+});
+
+module.exports = ROUTER;
